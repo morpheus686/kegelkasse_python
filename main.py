8bcfb3a92fd37e275e0c0dc4c67c399b25e747c3
Christopher Wurm <christopher_wurm@gmx.de>
2024-05-13 16:14:37 +0200

    Created Database

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+kivy.require('1.10.1')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class NeuralRandom(App):
+    def build(self):
+        return Label(text='Hello World')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+NeuralRandom().run()
