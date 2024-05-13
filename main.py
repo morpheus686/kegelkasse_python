e47adfd2a52664edbfb3ca63c3983ab030a378ff
Christopher Wurm <christopher_wurm@outlook.de>
2024-05-13 20:09:16 +0200

    added buildozer.spec and kv-file

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+import random
 
 kivy.require('1.10.1')
 
 
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
+
+
 class NeuralRandom(App):
     def build(self):
-        return Label(text='Hello World')
+        return MyRoot()
 
 
 NeuralRandom().run()
