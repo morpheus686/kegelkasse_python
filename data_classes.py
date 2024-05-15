7c20ced3a8235d70b660a448b8289c13ac5b3d18
Christopher Wurm <christopher_wurm@gmx.de>
2024-05-15 14:29:24 +0200

    Added dataclasses und databaseaccess.

---
+++ data_classes.py
@@ -0,0 +1,23 @@
+
+from dataclasses import dataclass
+
+
+@dataclass
+class SumPerPlayer:
+    date: str
+    team: str
+    player_name: str
+    penalty: float
+
+
+@dataclass
+class SumPerTeam:
+    team_name: str
+    penalty_sum: float
+
+
+@dataclass
+class SumPerGame:
+    date: str
+    team_name: str
+    penalty_sum: float
---
+++ database.py
@@ -0,0 +1,24 @@
+import sqlite3
+from data_classes import SumPerPlayer
+
+
+class DatabaseAccess:
+    def __init__(self):
+        self._connection: sqlite3.Connection = sqlite3.connect('strafenkatalog.db')
+
+    def execute_query(self, sql: str, params: tuple = ()):
+        cursor = self._connection.cursor()
+        return cursor.execute(sql, params).fetchall()
+
+    def execute_command(self, sql: str, params: tuple = ()):
+        cursor = self._connection.cursor()
+        cursor.execute(sql, params)
+        self._connection.commit()
+
+
+database = DatabaseAccess()
+result = database.execute_query("SELECT * FROM SumPerPlayer")
+
+player_list = [SumPerPlayer(date=row[0], team=row[1], player_name=row[2], penalty=row[3]) for row in result]
+print(player_list)
+
